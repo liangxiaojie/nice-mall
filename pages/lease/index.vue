4bef95215164d3e4680d30455d1fd7ec70570ec2
liangxiaojie <15201515601@163.com>
2018-05-12 17:01:53 +0800

    leases

--- pages/lease/index.vue
+++ pages/lease/index.vue
@@ -13,38 +13,21 @@
 import leaseList from '~/components/leaseList'
 import appFooter from '~/components/appFooter'
 
+import { getLeases } from '~/apollo/goods'
+
 export default {
   components: {
     leaseList,
     appFooter
   },
-  data: function() {
+  async asyncData (context) {
+    let client = context.app.apolloProvider.defaultClient
+    const { leases } = await getLeases(client)
+
     return {
-      leases: [{
-        imgSrc: 'http://hhfanyi.com/uploads/20171215/4e27558ef030409a07160fca2601712f.jpg',
-        title: '科大讯飞翻译机晓译翻译器同声多国语言出国旅游智能翻译神器',
-        discription: '【中英离线翻译】支持中英/中日/中韩/中法/中西即时语音互译，出国就要畅快聊！',
-        price: 30,
-        priceUnit: {
-          type: 'day',
-          displayName: '天'
-        },
-        mark: 5,
-        sales: 5
-      }, {
-        imgSrc: 'http://hhfanyi.com/uploads/20180110/e75a7fd4e78b478f4e210e72199ba5e5.jpg',
-        title: '百度翻译机',
-        discription: '美国WiFi翻译一体机(中英翻译）',
-        price: 0,
-        priceUnit: {
-          type: 'day',
-          displayName: '天'
-        },
-        mark: 5,
-        sales: 5
-      }]
+      leases,
     }
-  }
+  },
 }
 </script>
 
