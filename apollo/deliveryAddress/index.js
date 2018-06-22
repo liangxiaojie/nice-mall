2777113c2ddf3d27627eb7c1ffc6d33ddfda9937
liangxiaojie <15201515601@163.com>
2018-06-23 00:25:09 +0800

    updateDeliveryAddress

--- apollo/deliveryAddress/index.js
+++ apollo/deliveryAddress/index.js
@@ -1,5 +1,6 @@
 import deliveryAddressesGQL from './deliveryAddresses.gql'
 import createDeliveryAddressGQL from './createDeliveryAddress.gql'
+import updateDeliveryAddressGQL from './updateDeliveryAddress.gql'
 
 export async function getDeliveryAddresses (client) {
   const {data} = await client.query({
@@ -11,6 +12,14 @@ export async function getDeliveryAddresses (client) {
 export async function createDeliveryAddress(client, deliveryAddress) {
   const { data } = await client.mutate({
     mutation: createDeliveryAddressGQL,
+    variables: deliveryAddress,
+  });
+  return data;
+}
+
+export async function updateDeliveryAddress(client, deliveryAddress) {
+  const { data } = await client.mutate({
+    mutation: updateDeliveryAddressGQL,
     variables: deliveryAddress,
   });
   return data;
